be8db2c2deafcc65890288bd3b8eeed49c61ccbe
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2019-06-26 18:25:08 +0200

    Add create event API endpoint

--- src/main/kotlin/io/sixtysix/happenings/controllers/EventsController.kt
+++ src/main/kotlin/io/sixtysix/happenings/controllers/EventsController.kt
@@ -2,10 +2,13 @@ package io.sixtysix.happenings.controllers
 
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
+import io.sixtysix.models.NewEvent
 import io.sixtysix.services.EventService
 
 fun Route.eventsController(eventService: EventService) {
@@ -15,6 +18,12 @@ fun Route.eventsController(eventService: EventService) {
         get("/") {
             val events = eventService.getAllEvents()
             call.respond(events)
+        }
+
+        post("/") {
+            val event = call.receive<NewEvent>()
+            eventService.createEvent(event)
+            call.respond(HttpStatusCode.Created)
         }
 
         get("/{id}") {
