cf03dcd283e04c32143976af11b6a779ec561ac7
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2019-08-10 17:23:25 +0200

    Increase test coverage in events controller

--- src/test/kotlin/io/sixtysix/happenings/controllers/EventsControllerTest.kt
+++ src/test/kotlin/io/sixtysix/happenings/controllers/EventsControllerTest.kt
@@ -1,14 +1,53 @@
 package io.sixtysix.happenings.controllers
 
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
+import io.sixtysix.happenings.models.Event
+import io.sixtysix.happenings.utils.DateTimeAdapter
+import org.joda.time.DateTime
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class EventsControllerTest : ControllerTest {
+
+    @Test
+    fun `Return all events`() = testApplication {
+        handleRequest(HttpMethod.Get, "/api/events") {
+            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+        }.apply {
+            val listType = object : TypeToken<List<Event>>() {}.type;
+            val events = getGsonInstance().fromJson<List<Event>>(response.content, listType)
+
+            assertEquals(HttpStatusCode.OK, response.status())
+            assertEquals(3, events.size)
+        }
+    }
+
+    @Test
+    fun `Return single event`() = testApplication {
+        handleRequest(HttpMethod.Get, "/api/events/1") {
+            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+        }.apply {
+            val event = getGsonInstance().fromJson(response.content, Event::class.java)
+
+            assertEquals(HttpStatusCode.OK, response.status())
+            assertEquals(1, event.id)
+        }
+    }
+
+    @Test
+    fun `Return 404 Not Found when single event does not exist`() = testApplication {
+        handleRequest(HttpMethod.Get, "/api/events/42") {
+            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+        }.apply {
+            assertEquals(HttpStatusCode.NotFound, response.status())
+        }
+    }
 
     @Test
     fun `Return 401 Unauthorized when unauthenticated`() = testApplication {
@@ -54,4 +93,7 @@ class EventsControllerTest : ControllerTest {
             assertEquals(HttpStatusCode.NoContent, response.status())
         }
     }
+
+    private fun getGsonInstance() =
+        GsonBuilder().registerTypeAdapter(DateTime::class.java, DateTimeAdapter()).create()
 }
