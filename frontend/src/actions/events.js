297db8fe6cae08561bb4b690fc8249ed06d86113
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2019-09-26 16:27:50 +0200

    Remove unused async keyword

--- frontend/src/actions/events.js
+++ frontend/src/actions/events.js
@@ -2,7 +2,7 @@ import { createAction } from 'redux-starter-kit';
 
 export const setEvents = createAction('events/setEvents');
 
-export const fetchEvents = () => async dispatch => {
+export const fetchEvents = () => dispatch => {
   fetch('/api/events')
     .then(res => res.json())
     .then(res => dispatch(setEvents(res)));
