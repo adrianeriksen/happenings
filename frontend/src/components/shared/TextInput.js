081469c5eb6e2fe4cf4ad1fe9d254d60edadf304
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2019-11-06 18:53:23 +0100

    Replace Formic with React Hook Forms

--- frontend/src/components/shared/TextInput.js
+++ frontend/src/components/shared/TextInput.js
@@ -1,27 +1,10 @@
 import React from 'react';
 
-function TextInput({
-  id,
-  label,
-  name,
-  value,
-  handleChange,
-  handleBlur,
-  type = 'text',
-  ...rest
-}) {
+function TextInput({ id, label, name, register, type = 'text', ...rest }) {
   return (
     <div className="form-group">
-      <label for={id}>{label}</label>
-      <input
-        id={id}
-        type={type}
-        name={name}
-        value={value}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        {...rest}
-      />
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type={type} name={name} ref={register} {...rest} />
     </div>
   );
 }
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Formik } from 'formik';
+import useForm from 'react-hook-form';
 
 import { authenticate } from '../actions/auth';
 import TextInput from '../components/shared/TextInput';
@@ -10,23 +10,10 @@ function Login({ history, authenticate, isAuthenticated, isError }) {
     history.push('/');
   }
 
-  const initialValues = {
-    email: '',
-    password: ''
-  };
+  const { register, handleSubmit } = useForm();
 
-  const validate = values => {
-    const errors = {};
-
-    if (!values.email) errors.email = 'Please enter your e-mail';
-    if (!values.password) errors.password = 'Please enter your password';
-
-    return errors;
-  };
-
-  const onSubmit = (values, { setSubmitting }) => {
+  const onSubmit = values => {
     authenticate(values.email, values.password);
-    setSubmitting(false);
   };
 
   return (
@@ -34,36 +21,24 @@ function Login({ history, authenticate, isAuthenticated, isError }) {
       {isError && (
         <p>You have entered an invalid username and password combination.</p>
       )}
-      <Formik
-        initialValues={initialValues}
-        validate={validate}
-        onSubmit={onSubmit}
-      >
-        {({ values, handleChange, handleBlur, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <h2>Login</h2>
-            <TextInput
-              label="E-mail"
-              id="login-form-email"
-              name="email"
-              type="email"
-              value={values.email}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-            />
-            <TextInput
-              label="Password"
-              id="login-form-password"
-              name="password"
-              type="password"
-              value={values.password}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-            />
-            <button type="submit">Log In</button>
-          </form>
-        )}
-      </Formik>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Login</h2>
+        <TextInput
+          label="E-mail"
+          id="login-form-email"
+          name="email"
+          type="email"
+          register={register({ required: true })}
+        />
+        <TextInput
+          label="Password"
+          id="login-form-password"
+          name="password"
+          type="password"
+          register={register({ required: true })}
+        />
+        <button type="submit">Log In</button>
+      </form>
     </>
   );
 }
