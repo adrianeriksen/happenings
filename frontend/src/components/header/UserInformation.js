e01d808a47600666a45173c97c2bcb6f0c86cbc0
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2019-11-08 16:10:24 +0100

    Align navigation buttons to the right

--- frontend/src/components/header/UserInformation.js
+++ frontend/src/components/header/UserInformation.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const UserInformation = ({ user, deauthenticate }) => (
-  <>
+  <div class="user-information">
     <button type="button">{user.name}</button>
     <button type="button" onClick={deauthenticate}>
       Log out
     </button>
-  </>
+  </div>
 );
 
 export default UserInformation;
