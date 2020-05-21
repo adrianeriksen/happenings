a5b4ebbab1333869c6ceadda5f584f240733fa82
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2020-05-21 18:01:09 +0200

    Remove frontend

--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-
-import auth from './auth';
-import events from './events';
-
-export default combineReducers({
-  auth,
-  events
-});
--- frontend/src/utils/datetime.js
+++
@@ -1,9 +0,0 @@
-import { format } from 'date-fns';
-
-export default function DateTime(timestamp) {
-  const date = new Date(timestamp);
-
-  return {
-    toLongFormat: () => format(date, 'dd.MM.yyyy HH:mm')
-  };
-}
